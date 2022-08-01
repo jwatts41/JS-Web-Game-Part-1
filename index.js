473f9dfdf81bedf6767fe242c35387bc4e7e4f6c
jwatts41 <wattsjonovan@yahoo.com>
2022-07-31 22:24:49 -0400

    added background

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ function newImage(assets, left, bottom){
 }
 
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 350, 100)
-newImage('assets/well.png', 500, 425) 
 
 
 function newItem(assets, left, bottom){
@@ -30,8 +24,33 @@ function newItem(assets, left, bottom){
     })
 }
 
+
+
+function backdrop(assets, left, bottom, width, height) {
+    for(let i = 0; i < height; i++){
+        for(let j = 0; j < width; j++){
+            newImage(assets, left + j*100, bottom+i*100)
+        }
+    }
+}
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+backdrop('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+backdrop('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 350, 100)
+newImage('assets/well.png', 500, 425) 
+
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
-
-
